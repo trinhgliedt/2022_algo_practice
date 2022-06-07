5c745e9e6435de3cd408904ce87ecf5913b43a23
Trinh Gliedt <67726707+trinhgliedt@users.noreply.github.com>
2022-06-06 22:33:16 -0700

    two sums hash map

--- 1_Two_sums.js
+++ 1_Two_sums.js
@@ -36,13 +36,42 @@ const two_sums = function(nums, target) {
   // Runtime: 139 ms, faster than 35.74% of JavaScript online submissions for Two Sum.
 // Memory Usage: 42.5 MB, less than 68.31% of JavaScript online submissions for Two Sum.
 
+// let nums1 = [1, 3, 7, 9, 2], t1 = 11;
+// let nums2 = [1, 3, 7, 9, 2], t2 = 25;
+// let nums3 = [], t3 = 25;
+// let nums4 = [5], t4 = 5;
+// let nums5 = [1,6], t5 = 7;
+// console.log(two_sums(nums1, t1));
+// console.log(two_sums(nums2, t2));
+// console.log(two_sums(nums3, t3));
+// console.log(two_sums(nums4, t4));
+// console.log(two_sums(nums5, t5));
+
+// Space time complexity: On2
+
+function twoSumsHash(nums, target) {
+  const numberToFindHashMap = {};
+  for(let p1 = 0; p1 < nums.length; p1++) {
+    const numberToFind = target - nums[p1];
+    if (numberToFindHashMap.hasOwnProperty(nums[p1])){
+      return [numberToFindHashMap[nums[p1]],p1];
+    }
+    numberToFindHashMap[numberToFind] = p1;
+    // console.log(" nums[p1]:", nums[p1],"numberToFindHashMap",numberToFindHashMap,"numberToFind:",numberToFind);
+  }
+  return null;
+};
+// Leetcode submission result
+// Runtime: 85 ms, faster than 75.48% of JavaScript online submissions for Two Sum.
+// Memory Usage: 42.6 MB, less than 50.63% of JavaScript online submissions for Two Sum.
+
 let nums1 = [1, 3, 7, 9, 2], t1 = 11;
 let nums2 = [1, 3, 7, 9, 2], t2 = 25;
 let nums3 = [], t3 = 25;
 let nums4 = [5], t4 = 5;
 let nums5 = [1,6], t5 = 7;
-console.log(two_sums(nums1, t1));
-console.log(two_sums(nums2, t2));
-console.log(two_sums(nums3, t3));
-console.log(two_sums(nums4, t4));
-console.log(two_sums(nums5, t5));
+console.log(twoSumsHash(nums1, t1));
+console.log(twoSumsHash(nums2, t2));
+console.log(twoSumsHash(nums3, t3));
+console.log(twoSumsHash(nums4, t4));
+console.log(twoSumsHash(nums5, t5));
