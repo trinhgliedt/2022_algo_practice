a6e47837167b3eaa0bcc7f666bc43322a459277f
Trinh Gliedt <67726707+trinhgliedt@users.noreply.github.com>
2022-06-07 22:40:54 -0700

    Container with most water

--- 1_Two_sums.js
+++ 1_Two_sums.js
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/two-sum/submissions/
+
 // Given an array of integers, return the indices of the two numbers that add up to a given target.
 // function two_sums(nums, t) {
 //     // two pointers
@@ -18,22 +20,22 @@
 // Memory Usage: 42.5 MB, less than 68.31% of JavaScript online submissions for Two Sum.
 
 
-const two_sums = function(nums, target) {
-    for(let p1 = 0; p1 < nums.length; p1++) {
-      
-      const numberToFind = target - nums[p1];
-  
-      for(let p2 = p1 + 1; p2 < nums.length; p2++) {
-        if(numberToFind === nums[p2]) {
-          return [p1, p2];
-        }
+const two_sums = function (nums, target) {
+  for (let p1 = 0; p1 < nums.length; p1++) {
+
+    const numberToFind = target - nums[p1];
+
+    for (let p2 = p1 + 1; p2 < nums.length; p2++) {
+      if (numberToFind === nums[p2]) {
+        return [p1, p2];
       }
     }
-  
-    return null;
-  };
-  // Leetcode submission result
-  // Runtime: 139 ms, faster than 35.74% of JavaScript online submissions for Two Sum.
+  }
+
+  return null;
+};
+// Leetcode submission result
+// Runtime: 139 ms, faster than 35.74% of JavaScript online submissions for Two Sum.
 // Memory Usage: 42.5 MB, less than 68.31% of JavaScript online submissions for Two Sum.
 
 // let nums1 = [1, 3, 7, 9, 2], t1 = 11;
@@ -51,10 +53,10 @@ const two_sums = function(nums, target) {
 
 function twoSumsHash(nums, target) {
   const numberToFindHashMap = {};
-  for(let p1 = 0; p1 < nums.length; p1++) {
+  for (let p1 = 0; p1 < nums.length; p1++) {
     const numberToFind = target - nums[p1];
-    if (numberToFindHashMap.hasOwnProperty(nums[p1])){
-      return [numberToFindHashMap[nums[p1]],p1];
+    if (numberToFindHashMap.hasOwnProperty(nums[p1])) {
+      return [numberToFindHashMap[nums[p1]], p1];
     }
     numberToFindHashMap[numberToFind] = p1;
     // console.log(" nums[p1]:", nums[p1],"numberToFindHashMap",numberToFindHashMap,"numberToFind:",numberToFind);
@@ -70,9 +72,30 @@ let nums1 = [1, 3, 7, 9, 2], t1 = 11;
 let nums2 = [1, 3, 7, 9, 2], t2 = 25;
 let nums3 = [], t3 = 25;
 let nums4 = [5], t4 = 5;
-let nums5 = [1,6], t5 = 7;
+let nums5 = [1, 6], t5 = 7;
 console.log(twoSumsHash(nums1, t1));
 console.log(twoSumsHash(nums2, t2));
 console.log(twoSumsHash(nums3, t3));
 console.log(twoSumsHash(nums4, t4));
 console.log(twoSumsHash(nums5, t5));
+
+var twoSum = function (nums, target) {
+  const numsMap = {};
+
+  for (let p = 0; p < nums.length; p++) {
+    const currentMapVal = numsMap[nums[p]];
+
+    if (currentMapVal >= 0) {
+      return [currentMapVal, p];
+    } else {
+      const numberToFind = target - nums[p];
+      numsMap[numberToFind] = p;
+    }
+  }
+
+  return null;
+};
+
+// Leetcode submission result
+// Runtime: 73 ms, faster than 87.81% of JavaScript online submissions for Two Sum.
+// Memory Usage: 42.7 MB, less than 50.63% of JavaScript online submissions for Two Sum.
---
+++ 2_Container_with_most_water.js
@@ -0,0 +1,61 @@
+// https://leetcode.com/problems/container-with-most-water/
+
+// You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+// Find two lines that together with the x-axis form a container, such that the container contains the most water.
+// Return the maximum amount of water a container can store.
+
+// real question: find the maximum difference between x position multiplied by the height
+var maxArea = function(height) {
+    let maxA = 0, p1 = 0;
+    while (p1 <= height.length-2) {
+        let p2 = p1+1;
+        while (p2 <= height.length-1){
+            const area = Math.min(height[p1], height[p2])*(p2-p1);
+            if (area > maxA) maxA = area;
+            // console.log("height[p1]:", height[p1], ", height[p2]:", height[p2], ", maxA:", maxA);
+            p2++;
+        }
+        p1++;
+    }
+    return maxA;
+};
+// The function above works, but it's too slow and exceeded time limit on leetcode
+
+var maxArea = function(height) {
+    let maxWater = 0;
+    for (let p1 = 0; p1 < height.length; p1++) {
+        for (let p2 = p1 + 1; p2 < height.length; p2++){
+            const minHeight = Math.min(height[p1], height[p2]);
+            const water = minHeight*(p2-p1);
+            maxWater = Math.max(water, maxWater);
+        }
+    }
+    return maxWater;
+};
+// The function above works, but it's too slow and exceeded time limit on leetcode
+
+var maxArea = function(height) {
+    let maxWater = 0, i = 0, j = height.length-1;
+    while (i < j){
+        let minHeight = Math.min(height[i],height[j]);
+        let water = minHeight*(j-i);
+        maxWater = Math.max(water,maxWater);
+        if (height[i] < height[j]) i++;
+        else j--;
+    }
+    return maxWater;
+};
+// if value at i is smaller than j, move the smaller pointer forward, cos we want to keep the larger value to compare with the new value of the other pointer
+// Runtime: 87 ms, faster than 76.08% of JavaScript online submissions for Container With Most Water.
+// Memory Usage: 48.9 MB, less than 97.37% of JavaScript online submissions for Container With Most Water.
+
+let h1 = [7, 1, 2, 3, 9]; 
+//        0  1  2  3  4
+// length = 5
+let h2 = [];
+let h3 = [5];
+let h4= [1,8,6,2,5,4,8,3,7];
+console.log(maxArea(h1));
+// console.log(maxArea(h2));
+// console.log(maxArea(h3));
+console.log(maxArea(h4));
