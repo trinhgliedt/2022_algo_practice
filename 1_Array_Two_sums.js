6de9f7bd8cf4fd3e6cf4b36e12eefb26fa06e241
Trinh Gliedt <67726707+trinhgliedt@users.noreply.github.com>
2023-02-15 23:11:49 -0800

    two sum

--- 1_Array_Two_sums.js
+++ 1_Array_Two_sums.js
@@ -25,7 +25,7 @@ var twoSum = function(nums, target) {
     }
     return [-1,-1];
 };
-console.log(twoSum([2,7,11,15], 9));
+// console.log(twoSum([2,7,11,15], 9));
 // So the function above got a space complexity of O^1, but a time complexity of O(n^2)
 // This is because the function contains a nested loop that iterates through the array twice, resulting in an overall time complexity of O(n^2). Specifically, the outer loop iterates through each element of the array, while the inner loop iterates through the remaining elements to find a matching pair that sums to the target value.
 
@@ -33,3 +33,26 @@ console.log(twoSum([2,7,11,15], 9));
 // While iterating through the array, we'll save the value that's already seen
 // then, along the way, every step we take we'll look back into the dictionary to see if there's a match in the past values
 // So, the dictionary will need to contain the index and value of the seens
+var twoSum2 = function(nums, target){
+  // seen: object to store the values we've iterated to. 
+  // keys: the number, values:the index
+  // if there's more than one number of the same value, then it's ok to replace the old index with the new one
+  let seen = {};
+   for (let i=0; i<nums.length; i++){
+    // in each iteration, find out what the target paired value is
+    let targetValue = target - nums[i];
+    // then, look up the seen oabject. If the target is in there then return the value. Job is done
+    if (seen.hasOwnProperty(targetValue)){
+      return [seen[targetValue],i];
+    }
+    // ok, if we are here, then the seen object didn't have the target value.
+    // so now our job is to store the current value in the seen object
+    seen[nums[i]] = i;
+   }
+  //  if we're at the end of the iteration, that means we can't find any match.
+  return [-1,-1];
+}
+console.log(twoSum2([2,7,11,15], 9));
+// The time complexity of this function is O(n) because it iterates through the nums array once.
+
+// The space complexity is also O(n) because the seen object can potentially store all the elements in the nums array. In the worst case scenario where there are no two elements that add up to the target, the seen object will store all the elements in the nums array, which is equal to the space complexity of O(n).
